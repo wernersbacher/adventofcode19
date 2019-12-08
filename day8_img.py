c65411e2a9f28498817867fd532c16ac4c4f9bdf
Markus Wernersbach <wernersbacher@gmail.com>
2019-12-08 11:59:14 +0100

    day8: part 2 done

--- day8_img.py
+++ day8_img.py
@@ -10,10 +10,12 @@ PIXELS = WIDTH * HEIGHT
 LAYERS = int(len(imgstring)/PIXELS)
 
 print(PIXELS, LAYERS)
-# print(imgstring)
 
+layerList = []
 
 layer_with_fewest_zeros = 0
+
+print("PART 1: ------------------------")
 
 minimalZeroes = 0
 layerChecksum = 0
@@ -21,7 +23,8 @@ for i in range(LAYERS):
     start = i * PIXELS
     end = start + PIXELS
     current_layer = imgstring[start:end]
-    print(current_layer)
+    layerList.append(current_layer)
+    # print(current_layer)
 
     #the number of 1 digits multiplied by the number of 2 digits
     zeroCount = current_layer.count('0')
@@ -31,3 +34,27 @@ for i in range(LAYERS):
         layer_with_fewest_zeros = i
 
 print(layerChecksum)
+print("PART 2: ------------------------")
+print(layerList)
+
+processed_image = []
+
+for pixelIndex in range(PIXELS):
+    processed_image.insert(pixelIndex, 2)
+    print("NEXT PIXEL")
+    for layerIndex in range(LAYERS):
+        print("Current pixel in done picture: {}".format(processed_image[pixelIndex]))
+        if processed_image[pixelIndex] != 2:
+            break
+            #break # jump to next pixel if proc. image is already not transparent anymore
+
+        processed_image[pixelIndex] = int(layerList[layerIndex][pixelIndex])
+
+print(processed_image)
+
+print("Copy those as csv to excel")
+for i in range(PIXELS):
+    pixel = processed_image[i]
+    print(str(pixel)+";", end='', flush=True)
+    if (i+1) % WIDTH == 0:
+        print()
